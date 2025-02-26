2b32d32de3b71afaef967a4633074e8b136fe7c7
Ronald MacDonald <ronald@rmacd.com>
2025-02-26 23:30:08 +0000

    add about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import {useAuth} from "@espresso-lab/mantine-cognito";
+import {Button, Container, Group, Paper, Title, Text} from "@mantine/core";
+import Link from "next/link";
 
 export default function About() {
 
-    const { logout, userAttributes } = useAuth();
-
     return (
-        <div>About</div>
+        <Container size={"md"} py={"xl"}>
+
+            <Paper shadow={"xs"} p={"xl"} mb={"xl"}>
+                <Title order={2} mb="xs">About</Title>
+                <Text mb={"md"}>
+                    This is very much an alpha version. Don&#39;t store anything on here you wouldn&#39;t want your granny to read.
+                </Text>
+                <Text mb={"md"}>
+                    Please submit complaints or suggestions to <a href={"mailto:ron@docs.scot"}>ron@docs.scot</a>.
+                </Text>
+            </Paper>
+
+            <Group mt="lg">
+                <Button component={Link} href={"/"} variant="outline">Back</Button>
+            </Group>
+        </Container>
     )
 }
--- src/app/boards/[healthBoard]/sites/[siteId]/page.tsx
+++ src/app/boards/[healthBoard]/sites/[siteId]/page.tsx
@@ -106,7 +106,6 @@ export default function About() {
                 </Group>
             </Container>
 
-
         </>
     )
 
